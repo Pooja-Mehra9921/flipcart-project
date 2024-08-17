f2e1ded8ca644a97cdfd4bcbb68a42ede92f9691
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2024-08-17 23:14:24 +0530

    first commit

---
+++ flipcart-clone/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Homepage from "./src/pages/Homepage";
+import Loginpage from "./src/pages/Loginpage";
+
+const App =()=>{
+    return(
+        <>
+        <Loginpage/>
+        <Homepage/>
+        </>
+    );
+};
+
+export default App;
