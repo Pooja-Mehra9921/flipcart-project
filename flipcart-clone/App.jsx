3109b7146bb140b1dd5aeefb35d41793ac19643f
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2024-08-18 20:26:50 +0530

    making login page for flipkart

--- flipcart-clone/App.jsx
+++ flipcart-clone/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Homepage from "./src/pages/Homepage";
 import Loginpage from "./src/pages/Loginpage";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
 
 const App =()=>{
     return(
         <>
-        <Loginpage/>
-        <Homepage/>
+        <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<Homepage/>} />
+            <Route path="/homepage" element={<Homepage/>} />
+            <Route  path="/login" element={<Loginpage/>} />
+        </Routes>
+        </BrowserRouter>
+        
+        
         </>
     );
 };
--- flipcart-clone/src/pages/Loginpage/index.jsx
+++ flipcart-clone/src/pages/Loginpage/index.jsx
@@ -1,8 +1,22 @@
+import { Box, Grid, Paper } from "@mui/material";
 import React from "react";
+import FLIPCART_MAIN_IMAGE from "../../assets/images/login-page-image.jpg"
+import "./style.css"
 
 const Loginpage =()=>{
     return(
-        <></>
+        <>
+        <Box className="login-container">
+            <Paper elevation={20} className="login-paper">
+                <Grid container spacing={2}>
+<Grid item xs={12} sm={12} md={6} lg={6} className="fk-left-section">
+    <img src={FLIPCART_MAIN_IMAGE} alt="flipcart-login-image" />
+</Grid>
+<Grid item xs={12} sm={12} md={6} lg={6} className="fk-right-section"></Grid>
+                </Grid>
+            </Paper>
+        </Box>
+        </>
     );
 };
 
