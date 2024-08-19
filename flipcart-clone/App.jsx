f0ecd568f389366c46590adb5c4c4249c9a8d96e
Pooja-Mehra9921 <159164676+Pooja-Mehra9921@users.noreply.github.com>
2024-08-20 00:11:02 +0530

    flipcart changes

--- flipcart-clone/App.jsx
+++ flipcart-clone/App.jsx
@@ -1,22 +1,99 @@
-import React from "react";
-import Homepage from "./src/pages/Homepage";
-import Loginpage from "./src/pages/Loginpage";
-import { BrowserRouter , Routes, Route } from "react-router-dom";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomepageShimmer, ProductsShimmer } from "./src/components/Shimmer";
+import NotFoundPage from "./src/pages/NotFoundPage";
+const SelectedProduct = lazy(() => import("./src/pages/SelectedProduct"));
+const CartPage = lazy(() => import("./src/pages/CartPage"));
+const Homepage = lazy(() => import("./src/pages/Homepage"));
+const ProductsPage = lazy(() => import("./src/pages/ProductsPage"));
+const Login = lazy(() => import("./src/pages/Loginpage"));
 
-const App =()=>{
-    return(
-        <>
-        <BrowserRouter>
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
         <Routes>
-            <Route exact path="/" element={<Homepage/>} />
-            <Route path="/homepage" element={<Homepage/>} />
-            <Route  path="/login" element={<Loginpage/>} />
+          <Route
+            exact
+            path="/"
+            element={
+              <Suspense fallback={<HomepageShimmer />}>
+                <Homepage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/homepage"
+            element={
+              <Suspense fallback={<HomepageShimmer />}>
+                <Homepage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <Suspense fallback={<ProductsShimmer />}>
+                <Login />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/products"
+            element={
+              <Suspense fallback={<ProductsShimmer />}>
+                <ProductsPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/products/:category"
+            element={
+              <Suspense fallback={<ProductsShimmer />}>
+                <ProductsPage />
+              </Suspense>
+            }
+          />
+
+          <Route
+            path="/selected-product/:id"
+            element={
+              <Suspense fallback={<ProductsShimmer />}>
+                <SelectedProduct />
+              </Suspense>
+            }
+          />
+
+          <Route
+            path="/cart"
+            element={
+              <Suspense fallback={<ProductsShimmer />}>
+                <CartPage />
+              </Suspense>
+            }
+          />
+
+          <Route
+            path="/contact"
+            element={
+              <Suspense fallback={<ProductsShimmer />}>
+                <NotFoundPage />
+              </Suspense>
+            }
+          />
+
+          <Route
+            path="/about"
+            element={
+              <Suspense fallback={<ProductsShimmer />}>
+                <NotFoundPage />
+              </Suspense>
+            }
+          />
         </Routes>
-        </BrowserRouter>
-        
-        
-        </>
-    );
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
--- flipcart-clone/index.js
+++ flipcart-clone/index.js
@@ -1,11 +1,25 @@
 import React from "react";
-import ReactDom from "react-dom/client"
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root= ReactDom.createRoot((document.getElementById("root")));
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import "./style.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "./src/redux/store";
+import { PersistGate } from "redux-persist/es/integration/react";
+import BackdropLoader from "./src/components/BackdropLoader";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <>
-    <App/>
-    </>
+  <>
+    <Provider store={store}>
+      <PersistGate loading={<BackdropLoader />} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
+  </>
 );
